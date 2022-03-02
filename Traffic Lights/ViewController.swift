6175419cd688577d72ceb1d27eb2fe3b0cbd2cec
Andrey Sokolov <hi@andreysokolov.com>
2022-03-02 16:18:28 +0300

    Add code ViewController

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lightRedView: UIView!
+    @IBOutlet var lightYellowView: UIView!
+    @IBOutlet var lightGreenView: UIView!
+    
+    @IBOutlet var lightButtonSwitch: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
